b4d15103e10c812be67028a87b9c87d0f2ad6068
danilo.queiroz <danilo.queiroz@crptecnologia.com.br>
2021-05-29 15:59:08 -0300

    saving, listing and removing investimento

--- backend/my-invest/src/main/java/br/com/daniloqueiroz/resources/CategoriaResource.java
+++ backend/my-invest/src/main/java/br/com/daniloqueiroz/resources/CategoriaResource.java
@@ -3,17 +3,21 @@ package br.com.daniloqueiroz.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.daniloqueiroz.domain.Categoria;
 import br.com.daniloqueiroz.repository.CategoriaRepository;
 
 @RestController
+@RequestMapping("/categorias")
+@CrossOrigin(origins = "*")
 public class CategoriaResource {
 	
 	@Autowired
--- backend/my-invest/src/main/java/br/com/daniloqueiroz/resources/InvestimentoResource.java
+++ backend/my-invest/src/main/java/br/com/daniloqueiroz/resources/InvestimentoResource.java
@@ -3,6 +3,7 @@ package br.com.daniloqueiroz.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import br.com.daniloqueiroz.repository.InvestimentoRepository;
 
 @RestController
 @RequestMapping("/investimentos")
+@CrossOrigin(origins = "*")
 public class InvestimentoResource {
 	
 	@Autowired
